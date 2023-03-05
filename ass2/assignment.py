ebc6e9c7f0c3af977eceaf9a75a81c669870c1b6
FunnyPocketBook <ying-kai.dang@outlook.com>
2023-03-05 13:37:40 +0100

    Fix mesh, finishing touches
    - creates surface mesh correctly now
    - remove code that has been commented out
    - add method docstrings

--- ass2/assignment.py
+++ ass2/assignment.py
@@ -1,7 +1,6 @@
 import cv2
 import glm
 import numpy as np
-import copy
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
@@ -9,6 +8,8 @@ from engine.config import config
 
 from skimage import measure
 from skimage.draw import ellipsoid
+from scipy.ndimage.interpolation import zoom
+
 
 block_size = 1
 scalling_factor = 50
@@ -30,14 +31,40 @@ list_voxels = []
 list_list_points = []
 listout_of_bounds = []
 frame_counter = 0
+captures = {
+    "cam1": {
+        "cap": cv2.VideoCapture('ass2/data/cam1/video.avi'),
+        "background": cv2.imread('ass2/data/cam1/background.jpg'),
+    },
+    "cam2": {
+        "cap": cv2.VideoCapture('ass2/data/cam2/video.avi'),
+        "background": cv2.imread('ass2/data/cam2/background.jpg'),
+    },
+    "cam3": {
+        "cap": cv2.VideoCapture('ass2/data/cam3/video.avi'),
+        "background": cv2.imread('ass2/data/cam3/background.jpg'),
+    },
+    "cam4": {
+        "cap": cv2.VideoCapture('ass2/data/cam4/video.avi'),
+        "background": cv2.imread('ass2/data/cam4/background.jpg'),
+    },
+}
 
-# timers and counters
-set_voxel_positions_total_time = 0
-background_check_total_time = 0
-voxel_check_total_time = 0
-voxel_vis_total_time = 0
+
 
 def construct_voxel_space(step = 32, voxel_space_half_size = 1000):
+    """Generates the voxel space and stores it in a global variable.
+    The voxel space is a list of 3D points that represent the center of each voxel.
+    The voxel space is generated in the range [-voxel_space_half_size, voxel_space_half_size] in each dimension.
+    The step parameter controls the distance between voxels in the voxel space.
+    
+    Args:
+        step (int, optional): Distance between voxels in the voxel space. Defaults to 32.
+        voxel_space_half_size (int, optional): Half the size of the voxel space in each dimension. Defaults to 1000.
+        
+    Returns:
+        list: A list of 3D points that represent the center of each voxel.
+    """
     print("Generating voxel space...")
     camera_props = {}
     for cam in range(1,5):
@@ -65,14 +92,27 @@ def construct_voxel_space(step = 32, voxel_space_half_size = 1000):
     print("Done generating voxel space...")
     return list_voxels
 
+
 def check_voxel_visibility():
-    global frame_counter, background_check_total_time, voxel_check_total_time, voxel_vis_total_time
+    """Checks the visibility of each voxel in the voxel space.
+    
+    Returns:
+        data {list}: A list of 3D points that represent the center of each visible voxel.
+        colors {list}: A list of RGB colors that represent the color of each visible voxel.
+    """
+    global frame_counter
     data = []
     colors = []
     true_foregrounds = {}
     frames = {}
-    for cam in range(1,5):
-        true_foregrounds['cam'+str(cam)], frames['cam'+str(cam)] = subtract_background('cam'+str(cam))
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        futures = [executor.submit(subtract_background, 'cam'+str(cam)) for cam in range(1,5)]
+        for future in as_completed(futures):
+            foreground = future.result()[0]
+            frame = future.result()[1]
+            cam_id = future.result()[2]
+            true_foregrounds[cam_id] = foreground
+            frames[cam_id] = frame
         
     for i, voxel in enumerate(list_voxels):
         camera_counter = 0
@@ -89,7 +129,7 @@ def check_voxel_visibility():
             else:
                 break
         if camera_counter == 4:
-            data.append(list_voxels[i])
+            data.append(voxel)
             temp_rgb = np.float32([0.0,0.0,0.0])
             for i in range(4):
                 temp_rgb[0] += rgb_col[i][0]
@@ -100,14 +140,22 @@ def check_voxel_visibility():
             temp_rgb[2] = (temp_rgb[2] /4)/255
             colors.append(temp_rgb)
     frame_counter  += 1
-    print(frame_counter)
+    print(f"{frame_counter}")
     return data, colors
             
 
 def set_voxel_positions(width, height, depth):
-    global background_check_total_time, voxel_check_total_time, voxel_vis_total_time, frame_counter, set_voxel_positions_total_time
-    # Generates random voxel locations
-    # TODO: You need to calculate proper voxel arrays instead of random ones.
+    """Sets the position of the voxels in the voxel space.
+
+    Args:
+        width (int): The width of the voxel space.
+        height (int): The height of the voxel space.
+        depth (int): The depth of the voxel space.
+    
+    Returns:
+        data {list}: A list of 3D points that represent the center of each voxel.
+        colors {list}: A list of RGB colors that represent the color of each voxel.
+    """
     data = []
     colors = []
     xL = int(-width/2)
@@ -136,6 +184,15 @@ def set_voxel_positions(width, height, depth):
     return data, colors
 
 def get_camera_pos(rvecs, tvecs):
+    """Gets the position of the camera in the world coordinates.
+    
+    Args:
+        rvecs {list}: A list of rotation vectors.
+        tvecs {list}: A list of translation vectors.
+        
+    Returns:
+        cameraPosition {list}: A list of 3D points that represent the position of the camera in the world coordinates.
+    """
     rotM, j = cv2.Rodrigues(rvecs)
     cameraPosition = -np.matrix(rotM).transpose() * np.matrix(tvecs)
     #OpenCV Y down, Z forward meanwhile OpenGL uses Y for up so swap it
@@ -145,8 +202,11 @@ def get_camera_pos(rvecs, tvecs):
 
 
 def get_cam_positions():
-    # Generates dummy camera locations at the 4 corners of the room
-    # TODO: You need to input the estimated locations of the 4 cameras in the world coordinates.
+    """Gets the position of all cameras in the world coordinates.
+
+    Returns:
+        [list]: A list of 3D points that represent the position of all cameras in the world coordinates.
+    """
 
     #Cam1
     cam1M, cam1d, cam1rvecs, cam1tvecs = load_camera_properties('cam1')
@@ -168,6 +228,15 @@ def get_cam_positions():
             cam4pos],[[1.0,0,0],[0,1.0,0],[0,0,1.0],[1.0,1.0,0]]
 
 def get_extrensic_matrix(rvecs, tvecs):
+    """Gets the extrinsic matrix of the camera.
+    
+    Args:
+        rvecs {list}: A list of rotation vectors.
+        tvecs {list}: A list of translation vectors.
+    
+    Returns:
+        m44eye {list}: A 4x4 matrix that represents the extrinsic matrix of the camera.
+    """
     rv = rvecs.ravel()
     m44eye = np.identity(4)
     rotM, j = cv2.Rodrigues(rv)
@@ -175,8 +244,11 @@ def get_extrensic_matrix(rvecs, tvecs):
     return m44eye
 
 def get_cam_rotation_matrices():
-    # Generates dummy camera rotation matrices, looking down 45 degrees towards the center of the room
-    # TODO: You need to input the estimated camera rotation matrices (4x4) of the 4 cameras in the world coordinates.
+    """Gets the rotation matrices of all cameras.
+
+    Returns:
+        [list]: A list of 4x4 matrices that represent the rotation matrices of all cameras.
+    """
     #Cam1
     cam1M, cam1d, cam1rvecs, cam1tvecs = load_camera_properties('cam1')
     cam1M44 = get_extrensic_matrix(cam1rvecs, cam1tvecs)
@@ -201,6 +273,14 @@ def get_cam_rotation_matrices():
 
 #Create background image from all frames of videoclip. Each subsequent frame is slightly more transparent
 def get_background_image(path = 'ass2/data/cam1/'):
+    """Gets the background image of the video clip.
+    
+    Keyword Arguments:
+        path {str} -- The path to the video clip (default: {'ass2/data/cam1/'}).
+        
+    Returns:
+        average_image {ndarray} -- The background image.
+    """
     cap = cv2.VideoCapture(path + 'background.avi')
     ret, next_frame = cap.read()
     first_frame = next_frame
@@ -234,29 +314,30 @@ V_name = 'V'
 window_bar_name = 'Bars'
 
 def generate_mesh():
+    """Generates a surface mesh of the currently displayed image."""
     print('Generating mesh')
-    # Generate a level set about zero of two identical ellipsoids in 3D
-    #voxels = set_voxel_positions(config['world_width'], config['world_height'], config['world_width'])
-    #size = len(voxels)
-    ellip_base = ellipsoid(3, 3, 3, levelset=True)
-    #ellip_double = np.concatenate((ellip_base[:-1, ...],ellip_base[2:, ...]), axis=0)
-    voxels = set_voxel_positions(config['world_width'], config['world_height'], config['world_width'])
-    #data = np.zeros((len(voxels),len(voxels),len(voxels)), np.float32)
-    # input: z_coords, y_coords, x_coords
-    zint = []
-    yint = []
-    xint = []
-    for x in range(len(voxels)):
-        zint.append(np.floor(voxels[x][2]).astype(int))
-        yint.append(np.floor(voxels[x][1]).astype(int))
-        xint.append(np.floor(voxels[x][0]).astype(int))
-    shape = tuple([np.max(intcoords) +1 for intcoords in [zint, yint, xint]])
-    image = np.zeros(shape)
-    image[zint, yint, xint] += 1
+
+    voxels, _ = set_voxel_positions(config['world_width'], config['world_height'], config['world_width'])
+
+    min_x = min(voxel[0] for voxel in voxels)
+    min_y = min(voxel[1] for voxel in voxels)
+    min_z = min(voxel[2] for voxel in voxels)
+
+    shifted_voxels = [(voxel[0]-min_x, voxel[1]-min_y, voxel[2]-min_z) for voxel in voxels]
+
+    x_size = max(voxel[0] for voxel in shifted_voxels) + 1
+    y_size = max(voxel[1] for voxel in shifted_voxels) + 1
+    z_size = max(voxel[2] for voxel in shifted_voxels) + 1
+    data = np.zeros((x_size, z_size, y_size), dtype=np.uint8)
+
+    for voxel in shifted_voxels:
+        data[int(voxel[0]), int(voxel[2]), int(voxel[1])] = 1
+
+    data = zoom(data, 1.5)
 
     #data = np.reshape(voxels,(3,1))
     # Use marching cubes to obtain the surface mesh of these ellipsoids
-    verts, faces, normals, values = measure.marching_cubes(image,0, spacing=(1,1,1))
+    verts, faces, normals, values = measure.marching_cubes(data,0, spacing=(1,1,1))
 
     # Display resulting triangular mesh using Matplotlib. This can also be done
     # with mayavi (see skimage.measure.marching_cubes docstring).
@@ -268,45 +349,81 @@ def generate_mesh():
     mesh.set_edgecolor('k')
     ax.add_collection3d(mesh)
 
-    ax.set_xlabel("x-axis: a = 32")
-    ax.set_ylabel("y-axis: b = 32")
-    ax.set_zlabel("z-axis: c = 32")
-
-    ax.set_xlim(-32, 32)
-    ax.set_ylim(-32, 32)
-    ax.set_zlim(-32, 32)
+    ax.set_xlim(0, config['world_width'])
+    ax.set_zlim(0, config['world_height'])
+    ax.set_ylim(0, config['world_width'])
 
     plt.tight_layout()
     plt.show()
     print('Done generating mesh')
     return True
 
-#Load files from directory and subtract background from video
-#TODO: File loading should probablty be in a separate function so files are not loaded on each update
-#TODO: rewrite the function so it works like this:
-#On a single update in the scene 
-#Each camera calls this
-#Get the new frame from the video per camera
-#Perform background subtraction on the new frame
-#Return true foreground
-def subtract_background(cameraID = 'cam1'):
-    global frame_counter
-    path = 'ass2/data/' + cameraID + '/'
-    cap = cv2.VideoCapture(path + 'video.avi')
-    #fps = cap.get(cv2.CAP_PROP_FPS)
-    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    #duration = frame_count/fps
-    background_image = cv2.imread(path + 'background.jpg')
+
+def get_mask(frame, background_image):
+    """Returns a mask of the frame with the background subtracted
+    
+    Arguments:
+        frame {np.array} -- The frame to be processed
+        background_image {np.array} -- The background image
+
+    Returns:
+        [np.array] -- The mask of the frame
+    """
     background_imageHSV = cv2.cvtColor(background_image, cv2.COLOR_BGR2HSV)
     background_channels = cv2.split(background_imageHSV)
     kernelErode = np.ones((3, 3), np.uint8)
-    kernelDialate = np.ones((3, 3), np.uint8)
+    kernelDilate = np.ones((3, 3), np.uint8)
 
-    #Initialize UI elements
-    #cv2.namedWindow(window_bar_name)
-    #cv2.createTrackbar(H_name, window_bar_name , threshhold_h, 255, on_low_H_thresh_trackbar)
-    #cv2.createTrackbar(S_name, window_bar_name , threshhold_s, 255, on_low_S_thresh_trackbar)
-    #cv2.createTrackbar(V_name, window_bar_name , threshhold_v, 255, on_low_V_thresh_trackbar)
+    threshhold_h = 13
+    threshhold_s = 13
+    threshhold_v = 75
+    frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    blurred = cv2.GaussianBlur(frameHSV, (7, 7), 0)
+    frame_channels = cv2.split(blurred)
+
+    # Subtract background from frame
+    h = cv2.absdiff(background_channels[0], frame_channels[0])
+    s = cv2.absdiff(background_channels[1], frame_channels[1])
+    v = cv2.absdiff(background_channels[2], frame_channels[2])
+
+    #Threshold channels
+    _, h = cv2.threshold(h, threshhold_h, 255, cv2.THRESH_BINARY)
+    _, s = cv2.threshold(s, threshhold_s, 255, cv2.THRESH_BINARY)
+    _, v = cv2.threshold(v, threshhold_v, 255, cv2.THRESH_BINARY)
+
+    # Erode and dilate
+    h = cv2.erode(h, kernelErode, iterations=3)
+    h = cv2.dilate(h, kernelDilate, iterations=2)
+
+    s = cv2.erode(s, kernelErode, iterations=3)
+
+    v = cv2.erode(v, kernelErode, iterations=1)
+
+    #Combine channels
+    foreground = cv2.bitwise_or(h, s)
+    foreground = cv2.bitwise_or(foreground, v)
+    foreground = cv2.dilate(foreground, kernelDilate, iterations=2)
+
+    return foreground
+
+
+def subtract_background(camera_id = 'cam1'):
+    """Subtracts the background from the video and saves it to a file
+    
+    Keyword Arguments:
+        camera_id {str} -- The camera id (default: {'cam1'})
+    
+    Returns:
+        true_foreground {np.array} -- The mask of the frame
+        frame {np.array} -- The frame to be processed
+        camera_id {str} -- The camera id
+    """
+
+    global frame_counter, captures
+
+    cap = captures[camera_id]["cap"]
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    background_image = captures[camera_id]["background"]
     if  frame_counter > frame_count:
         frame_counter = 0
     cap.set(1,frame_counter)
@@ -314,58 +431,23 @@ def subtract_background(cameraID = 'cam1'):
     if not ret:
         print("Video error")
         return None
-    #cv2.imshow('Frame ', frame)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-    frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    blurred = cv2.GaussianBlur(frameHSV, (7, 7), 0)
-    frame_channels = cv2.split(blurred)
-
-    #H channel
-    temp_frame = cv2.absdiff(background_channels[0], frame_channels[0])
-    t1, im1 = cv2.threshold(temp_frame, threshhold_h, 255,  cv2.THRESH_BINARY)
-    cv2.erode(im1,kernelErode,im1)
-    cv2.erode(im1,kernelErode,im1)
-    cv2.erode(im1,kernelErode,im1)
-    cv2.dilate(im1,kernelDialate,im1)
-    cv2.dilate(im1,kernelDialate,im1)
-    #cv2.imshow('H ', im1)
-
-    #S channel
-    temp_frame = cv2.absdiff(background_channels[1], frame_channels[1])
-    t2, im2 = cv2.threshold(temp_frame, threshhold_s, 255,  cv2.THRESH_BINARY)
-    cv2.erode(im2,kernelErode,im2)
-    cv2.erode(im2,kernelErode,im2)
-    cv2.erode(im2,kernelErode,im2)
-    #cv2.imshow('S ', im2)
-    
-    #V channel
-    temp_frame = cv2.absdiff(background_channels[2], frame_channels[2])
-    t3, im3 = cv2.threshold(temp_frame, threshhold_v, 255,  cv2.THRESH_BINARY)
-    cv2.erode(im3,kernelErode,im3)
-    #cv2.imshow('V ', im3)
-    
-    true_foreground = cv2.bitwise_or(im1, im2)
-    true_foreground = cv2.bitwise_or(true_foreground, im3)
-    cv2.dilate(true_foreground,kernelDialate,true_foreground)
-    cv2.dilate(true_foreground,kernelDialate,true_foreground)
-    
-    #cv2.imshow('True foreground ', true_foreground)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-    #code commet incase i need it
-    #contours, hierarchy = cv2.findContours(foreground[1], cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE,)
-    #cv2.erode(foreground[1],kernelErode,foreground[1] )
-    #cv2.dilate(foreground[1],kernelDia,foreground[1] )
-    #cv2.drawContours(foreground[1], contours, -1, (0,255,0), 3)
-    # cv2.imshow('Frame ', frame)
-    # cv2.imshow('True foreground ', true_foreground)
-    # cv2.waitKey(0)
-    return true_foreground, frame
+    true_foreground = get_mask(frame, background_image)
+    return true_foreground, frame, camera_id
 
 #Load camera properties from folder directory
-def load_camera_properties(cameraID = 'cam1'):
-    path = 'ass2/data/' + cameraID + '/'
+def load_camera_properties(camera_id = 'cam1'):
+    """Loads the camera properties from the config.xml file
+    
+    Keyword Arguments:
+        camera_id {str} -- The camera id (default: {'cam1'})
+        
+    Returns:
+        camMatrix {np.array} -- The camera matrix
+        d {np.array} -- The distortion coefficients
+        rvecs {np.array} -- The rotation vectors
+        tvecs {np.array} -- The translation vectors
+    """
+    path = 'ass2/data/' + camera_id + '/'
     s = cv2.FileStorage(path + "config.xml", cv2.FILE_STORAGE_READ)
     camMatrix = s.getNode('CameraMatrix').mat()
     d = s.getNode('DistortionCoeffs').mat()
@@ -375,16 +457,31 @@ def load_camera_properties(cameraID = 'cam1'):
 
 #Slider events
 def on_low_H_thresh_trackbar(val):
+    """Sets the low H threshhold
+    
+    Arguments:
+        val {int} -- The value of the slider
+    """
     global threshhold_h
     threshhold_h = val
     threshhold_h = min(255, threshhold_h)
     cv2.setTrackbarPos(H_name, window_bar_name, threshhold_h)
 def on_low_S_thresh_trackbar(val):
+    """Sets the low S threshhold
+    
+    Arguments:
+        val {int} -- The value of the slider
+    """
     global threshhold_s
     threshhold_s = val
     threshhold_s = min(255, threshhold_s)
     cv2.setTrackbarPos(S_name, window_bar_name, threshhold_s)
 def on_low_V_thresh_trackbar(val):
+    """Sets the low V threshhold
+    
+    Arguments:
+        val {int} -- The value of the slider
+    """
     global threshhold_v
     threshhold_v = val
     threshhold_v = min(255, threshhold_v)
